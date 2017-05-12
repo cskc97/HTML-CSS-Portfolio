421e03b3c5f309cc28d78f903a9fcbcc8609cd75
Santhoshkrishnachaitanya Chelikavada <everythingskc@gmail.com>
2017-05-12 23:23:41 +0530

    stuff

---
+++ portfolio/login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Testing Stuff</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+
+<div class="container">
+
+    <div class="row">
+        <div class="col-md-12">
+
+            <form action="admin.php" method="post">
+                <div class="form-group">
+                    <label for="email">Email address:</label>
+                    <input type="email" class="form-control" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="pwd">Password:</label>
+                    <input type="password" class="form-control" id="pwd">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+
+
+
+
+        </div>
+    </div>
+
+</div>
+
+
+
+
+
+
+</body>
+
+
+</html>
